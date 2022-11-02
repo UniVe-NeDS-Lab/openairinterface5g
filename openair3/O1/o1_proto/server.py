980316833339e642822ba6d60b8882c79b9c9b96
Gabriele Gemmi <gemmi.g@northeastern.edu>
2022-11-02 16:02:44 -0400

    O1 server support for FM messages

--- openair3/O1/o1_proto/server.py
+++ openair3/O1/o1_proto/server.py
@@ -7,40 +7,19 @@ import random
 import requests
 import time
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 5000  # Port to listen on (non-privileged ports are > 1023)
-
 
 MR_PATH = "/events/unauthenticated.IAB_MEASUREMENTS"
-FAULT_ID = "28"
 
 cqiMessage = {
     "event": {
         "commonEventHeader": {
             "domain": "measurements",
-            "eventId": "nt:network-topology/nt:topology/nt:node/nt:node-id",
-            "eventName": "IAB_Measurement_Report",
-            "eventType": "MeasurementsReport",
-            "sequence": 0,
-            "priority": "Normal",
-            "reportingEntityId": "SDNR",
-            "reportingEntityName": "@controllerName@",
-            "sourceId": "",
+            "eventId": "",
             "sourceName": "O-RU-ID",
-            "startEpochMicrosec": "@timestamp@",
-            "lastEpochMicrosec": "@timestamp@",
-            "nfNamingCode": "",
-            "nfVendorName": "ietf-hardware (RFC8348) /hardware/component[not(parent)][1]/mfg-name",
-            "timeZoneOffset": "+00:00",
-            "version": "4.1",
-            "vesEventListenerVersion": "7.2.1"
+            "ts": 0,
         },
-        "measField": {
-            'cell': '',
-            'rssi': 0,
-            'snr': 0,
-            'rsrq': 0
-        }
+        "measField": {},
+        "failField": {}
     }
 }
 
@@ -52,16 +31,43 @@ class CQISimulator():
     def report_cqi(self, o1_msg: O1MeasurementReport):
         # o_ru_id = "ERICSSON-O-RU-1122" + str(random_time)
         print(f"Sending O1Msg: {o1_msg}")
-        # msg_as_json = cqiMessage
-        # msg_as_json["event"]["commonEventHeader"]["sourceName"] = o1_msg.imsi
-        # msg_as_json["event"]["measField"]["cell"] = 'test'
-        # msg_as_json["event"]["measField"]["rx_power_avg"] = o1_msg.rx_power_avg
-        # msg_as_json["event"]["measField"]["rx_power_tot"] = o1_msg.rx_power_tot
-        # msg_as_json["event"]["measField"]["n0_power_avg"] = o1_msg.n0_power_avg
-        # msg_as_json["event"]["measField"]["rx_rssi_dBm"] = o1_msg.rx_rssi_dBm
-        # msg_as_json["event"]["measField"]["ssb_rsrp_dBm"] = o1_msg.ssb_rsrp_dBm
-        # msg_as_json["event"]["measField"]["mcs"] = o1_msg.mcs
-        #sendPostRequest(self.mr_url, msg_as_json)
+        msg_as_json = cqiMessage
+        msg_as_json["event"]["commonEventHeader"]["sourceName"] = socket.getfqdn()
+        msg_as_json["event"]["commonEventHeader"]["ts"] = time.time()
+        if o1_msg.HasField('ue_msg'):
+
+            msg_as_json["event"]["commonEventHeader"]["eventId"] = 'IAB.MT.MeasureReport'
+            msg_as_json["event"]["measField"]["rnti"] = o1_msg.ue_msg.rnti
+            msg_as_json["event"]["measField"]["imsi"] = o1_msg.ue_msg.imsi
+            msg_as_json["event"]["measField"]["rx_power_avg"] = o1_msg.ue_msg.rx_power_avg
+            msg_as_json["event"]["measField"]["rx_power_tot"] = o1_msg.ue_msg.rx_power_tot
+            msg_as_json["event"]["measField"]["n0_power_avg"] = o1_msg.ue_msg.n0_power_avg
+            msg_as_json["event"]["measField"]["rx_rssi_dBm"] = o1_msg.ue_msg.rx_rssi_dBm
+            msg_as_json["event"]["measField"]["ssb_rsrp_dBm"] = o1_msg.ue_msg.ssb_rsrp_dBm
+            msg_as_json["event"]["measField"]["mcs"] = o1_msg.ue_msg.mcs
+        elif o1_msg.HasField('gnb_msg'):
+            msg_as_json["event"]["commonEventHeader"]["eventId"] = 'IAB.DU.MeasureReport'
+            msg_as_json["event"]["measField"]["rnti"] = o1_msg.gnb_msg.rnti
+            msg_as_json["event"]["measField"]["avg_rsrp"] = o1_msg.gnb_msg.avg_rsrp
+            msg_as_json["event"]["measField"]["srs_wide_band_snr"] = o1_msg.gnb_msg.srs_wide_band_snr
+            msg_as_json["event"]["measField"]["dlsch_mcs"] = o1_msg.gnb_msg.dlsch_mcs
+            msg_as_json["event"]["measField"]["ulsch_mcs"] = o1_msg.gnb_msg.ulsch_mcs
+            msg_as_json["event"]["measField"]["cqi"] = o1_msg.gnb_msg.cqi
+            msg_as_json["event"]["measField"]["dlsch_bler"] = o1_msg.gnb_msg.dlsch_bler
+            msg_as_json["event"]["measField"]["ulsch_bler"] = o1_msg.gnb_msg.ulsch_bler
+        elif o1_msg.HasField('ulf'):
+            msg_as_json["event"]["commonEventHeader"]['domain'] = 'failure'
+            msg_as_json["event"]["commonEventHeader"]["eventId"] = 'IAB.DU.ULSCH.FailureReport'
+            msg_as_json["event"]["measField"]["rnti"] = o1_msg.ulf.rnti
+            msg_as_json["event"]["measField"]["failure"] = o1_msg.ulf.failure
+        elif o1_msg.HasField('rrcf'):
+            msg_as_json["event"]["commonEventHeader"]['domain'] = 'failure'
+            msg_as_json["event"]["commonEventHeader"]["eventId"] = 'IAB.DU.RRC.FailureReport'
+            msg_as_json["event"]["measField"]["rnti"] = o1_msg.rrcf.rnti
+            msg_as_json["event"]["measField"]["failure"] = o1_msg.rrcf.failure
+        else:
+            print("Unkown message type")
+        sendPostRequest(self.mr_url, msg_as_json)
 
 
 def sendPostRequest(url, msg):
@@ -73,9 +79,9 @@ def sendPostRequest(url, msg):
         print(e)
 
 
-mr_host = os.getenv("MR-HOST", "http://10.75.11.18")
+mr_host = os.getenv("MR_HOST", "http://10.75.11.18")
 print("Using MR Host from os: " + mr_host)
-mr_port = os.getenv("MR-PORT", "3904")
+mr_port = os.getenv("MR_PORT", "3904")
 print("Using MR Port from os: " + mr_port)
 mr_url = mr_host + ":" + mr_port + MR_PATH
 CSim = CQISimulator(mr_url)
